58874bc36d2c4b92befec6efc09ac0de8baf8573
jix <josagoro2000@gmail.com>
2023-01-09 16:03:07 +0100

    ajout de la fonction population pour un pays donné

--- src/main/java/monprojet/dao/CountryRepository.java
+++ src/main/java/monprojet/dao/CountryRepository.java
@@ -11,5 +11,8 @@ import monprojet.entity.Country;
 // This will be AUTO IMPLEMENTED by Spring 
 
 public interface CountryRepository extends JpaRepository<Country, Integer> {
-
+    @Query(value = "SELECT SUM(population)"
+            +" FROM city"
+            +" WHERE country_id = :paysId", nativeQuery = true)
+    public Integer populationPourPays(Integer paysId);
 }
